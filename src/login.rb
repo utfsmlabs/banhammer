a9d13890bfd7e815da3576fb1a34c8f21323fbc8
Joaquín Muñoz M <Chekosoft@users.noreply.github.com>
2015-04-08 03:00:32 -0300

    Logins como corresponden

--- src/login.rb
+++ src/login.rb
@@ -20,8 +20,6 @@ Login.define do
             primos = Cuba.settings[:banhammer]['Primos']
 
             @login_form.save do |form|
-                ldap_clt = Net::LDAP.new :host => ldap_cfg['server'], :port => ldap_cfg['port']
-                filter = Net::LDAP::Filter.eq 'uid', form[:username]
 
                 unless primos.include? form[:username]
                     @error_message = "No eres primo. Deberíamos banearte >:C"
@@ -30,19 +28,36 @@ Login.define do
                     halt(res.finish)
                 end
 
+                ldap_clt = Net::LDAP.new :host => ldap_cfg['server'],
+                                    :port => ldap_cfg['port'],
+                                    :auth => {
+                                        :method => :simple,
+                                        :username => ldap_cfg['admin_dn'],
+                                        :password => ldap_cfg['admin_password']
+                                    }
+
+                filter = Net::LDAP::Filter.eq 'uid', form[:username]
+
                 user_dn = nil
                 ldap_clt.search :base => ldap_cfg['basedn'], :filter => filter do |result|
                     user_dn = result.dn
                 end
 
-                ldap_clt.auth user_dn, form[:password]
-                unless ldap_clt.bind
-                    @error_message = "Revisa bien la contraseña. Quizás fuiste baneado."
-                    res.status = 401
+                #ldap_clt.auth user_dn, form[:password]
+                begin
+                    unless ldap_clt.bind_as :base => user_dn, :password => form[:password]
+                        @error_message = "Revisa bien la contraseña. Quizás fuiste baneado."
+                        res.status = 401
+                        render 'login'
+                        halt(res.finish)
+                    else
+                        session[:admin] = form[:username]
+                    end
+                rescue BindingInformationInvalidError
+                    @error_message = "Al parecer no hay acceso al servidor LDAP :C"
+                    res.status = 500
                     render 'login'
                     halt(res.finish)
-                else
-                    session[:admin] = form[:username]
                 end
                 res.redirect '/ban'
             end
